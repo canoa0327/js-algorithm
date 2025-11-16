992200081e08cae2d9d3fb09a8a61e19a948a792
canoa0327 <canoa950327@gmail.com>
2025-11-17 01:47:51 +0900

    5-7

---
+++ section5/5-7.js
@@ -0,0 +1,19 @@
+function solution(str1, str2) {
+  let answer = "YES";
+  let sH = new Map();
+  if (str1.length !== str2.length) return "NO";
+  for (let x of str1) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+  console.log(sH);
+  for (let x of str2) {
+    if (!sH.has(x) || sH.get(x) == 0) return "NO";
+    sH.set(x, sH.get(x) - 1);
+  }
+  return answer;
+}
+
+let str1 = "abaCa";
+let str2 = "Caaab";
+console.log(solution(str1, str2));
