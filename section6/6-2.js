57bba0390a36a729b96d80842f422ef71aae6c62
canoa0327 <canoa950327@gmail.com>
2025-11-19 10:40:16 +0900

    6-2

---
+++ section6/6-2.js
@@ -0,0 +1,12 @@
+function solution(str) {
+  let answer = "";
+  let stack = [];
+  for (x of str) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else stack.push(x);
+  }
+  answer = stack.join("");
+  return answer;
+}
+console.log(solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
